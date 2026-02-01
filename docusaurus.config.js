49a3a38dd518341f13296b381dd1b75baca4fb5b
Ladbaby <1390265970@qq.com>
2026-02-01 09:32:17 +0800

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://ladbaby.github.io',
+  url: 'https://Ladbaby.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/PyOmniTS-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
